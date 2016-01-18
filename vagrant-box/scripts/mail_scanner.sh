3b2f3e905a9d30c266ac35f9ed0138e840d18659
Jakub Blaszczyk | hybris <jakub.blaszczyk@hybris.com>
2016-01-18 18:04:09 +0100

    reduced verbose, fixed the script ordering

--- vagrant-box/scripts/mail_scanner.sh
+++ vagrant-box/scripts/mail_scanner.sh
@@ -4,10 +4,13 @@
 apt-get update
 # Install lightweight antivirus scanner
 apt-get -y install clamav-daemon
-# Setup socket for scanner
-echo "TCPSocket 3310\nTCPAddr localhost" | tee -a /etc/clamav/clamd.conf
 # Get malware list information
 echo "----> Installing malware definition. This may take some time"
 freshclam --quiet
+# Setup socket for scanner
+cat >> /etc/clamav/clamd.conf << EOF
+TCPSocket 3310
+TCPAddr localhost
+EOF
 # Restart service
 service clamav-daemon restart
